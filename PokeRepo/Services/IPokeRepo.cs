78d72c8ab6322c342fc4ccc3cb39ded3dbda48e8
EmmaDQ <sydemma@hotmail.com>
2024-02-07 13:16:47 +0100

    add interface and class for services db

--- PokeRepo/Services/IPokeRepo.cs
+++ PokeRepo/Services/IPokeRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace PokeRepo.Services
+﻿using PokeRepo.Models;
+
+namespace PokeRepo.Services
 {
     public interface IPokeRepo
     {
+        public IEnumerable<Pokemon> GetPokemon();
+        public Pokemon GetByName(string name);
+        public Pokemon AddPokemonToDb(Pokemon pokemon);
     }
 }
