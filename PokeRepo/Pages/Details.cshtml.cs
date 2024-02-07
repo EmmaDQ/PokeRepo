cdf716001e69dd46d10468f14966c8e03e89544e
EmmaDQ <sydemma@hotmail.com>
2024-02-07 14:54:44 +0100

    add pages and detail design

--- PokeRepo/Pages/Details.cshtml.cs
+++ PokeRepo/Pages/Details.cshtml.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PokeRepo.Models;
 
 namespace PokeRepo.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        public Pokemon poke { get; set; } = new Pokemon();
+        public void OnGet(Pokemon poke)
         {
+            this.poke = poke;
         }
+
+        public void OnPost()
+        {
+
+        }
+
     }
 }
