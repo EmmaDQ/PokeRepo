c4f384d62b7a7661b3dba1043dc97a786c2c0fb5
Oreazuz <Omaddams@hotmail.com>
2024-02-08 13:48:34 +0100

    stash

--- PokeRepo/Pages/Details.cshtml.cs
+++ PokeRepo/Pages/Details.cshtml.cs
@@ -1,50 +1,20 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PokeRepo.Api;
 using PokeRepo.Models;
-using PokeRepo.Services;
 
 namespace PokeRepo.Pages
 {
     public class DetailsModel : PageModel
     {
-        public IPokkeRepo repo;
-        public ApiCaller caller;
-        public List<Pokemon> Pokemons { get; set; } = new();
 
-        public Pokemon Poke { get; set; } = new Pokemon();
+        public Pokemon Poke { get; set; }
+        public ApiCaller caller { get; set; }
 
-        public DetailsModel(ApiCaller caller, IPokkeRepo repo)
+
+        public async void OnGet(Pokemon pokemon)
         {
-            this.caller = caller;
-            this.repo = repo;
-        }
 
-        public DetailsModel()
-        {
-            this.caller = new ApiCaller();
-        }
-
-
-
-        public async void OnGet(string name)
-        {
-            Pokemons = await caller.GetFirst30Pokemon();
-
-            try
-            {
-                Pokemon? foundPoke = Pokemons.FirstOrDefault(p => p.Name == name);
-
-                if (foundPoke != null)
-                {
-                    Poke = foundPoke;
-                    repo.AddPokemonToDb(foundPoke);
-                }
-
-            }
-            catch (Exception ex)
-            {
-
-            }
+            Poke = pokemon;
 
 
         }
--- PokeRepo/Pages/Gen1.cshtml.cs
+++ PokeRepo/Pages/Gen1.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PokeRepo.Api;
+using PokeRepo.Models;
 
 namespace PokeRepo.Pages
 {
     public class Gen1Model : PageModel
     {
         public string? Name { get; set; }
-
+        public ApiCaller caller { get; private set; }
+        public Pokemon Poke { get; private set; }
 
         public Gen1Model()
         {
@@ -16,10 +19,12 @@ namespace PokeRepo.Pages
 
         }
 
-        public void OnPost()
+        public async Task OnPostAsync()
         {
+            string name =
 
-
+            caller = new ApiCaller();
+            Poke = await caller.MakeCall(name.ToLower());
 
         }
     }
